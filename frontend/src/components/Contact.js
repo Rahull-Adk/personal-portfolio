ddf733cf98ae8e80819f0710e2dbb9c7b5d9392d
Rahull-Adk <raahulladk@gmail.com>
2024-10-31 16:18:41 +0700

    Updated the backend url

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -26,13 +26,16 @@ export const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Sending...");
-    const response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(formDetails),
-    });
+    const response = await fetch(
+      "https://personal-portfolio-phi-umber.vercel.app/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(formDetails),
+      }
+    );
     setButtonText("Send");
     let result = await response.json();
     setFormDetails(formInitialDetails);
